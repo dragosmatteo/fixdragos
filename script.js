e69555c02c409d4d604d1d3b532631dc9eb15e54
Dragoș Mateo Dămoc <56765258+dragosmatteo@users.noreply.github.com>
2021-06-20 18:01:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
 // !! Dragos Matteo 5.06.2021 Folio
-
-// const navbar =  document.querySelector('.navbar');
-// let sticky = navbar.getBoundingClientRect().height;
-
-// // !! Sticky navigation
-// const navbarScroll = () => {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add('sticky')
-//   } else {
-//     navbar.classList.remove('sticky');
-//   }
-// };
-
-// window.onscroll = navbarScroll;
-
 // !! Modal 
 
 const modal = document.querySelector('#simpleModal');
